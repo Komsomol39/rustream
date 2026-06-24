a76c8e48c66f3c50ec589fb09f7dbae7e807ca9e
Komsomol39 <100467630+Komsomol39@users.noreply.github.com>
2026-06-24 23:32:08 +0200

    init

---
+++ app/src/main/java/com/komsomol/rustream/ui/theme/Theme.kt
@@ -0,0 +1,30 @@
+package com.komsomol.rustream.ui.theme
+
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Color(0xFF6750A4),
+    secondary = Color(0xFF625B71),
+    tertiary = Color(0xFF7D5260),
+    background = Color(0xFF1C1B1F),
+    surface = Color(0xFF1C1B1F),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color(0xFFE6E1E5),
+    onSurface = Color(0xFFE6E1E5),
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = Color(0xFF6750A4),
+    secondary = Color(0xFF625B71),
+    tertiary = Color(0xFF7D5260),
+)
+
+@Composable
+fun RuStreamTheme(darkTheme: Boolean = true, content: @Composable () -> Unit) {
+    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
+    MaterialTheme(colorScheme = colorScheme, typography = Typography(), content = content)
+}
