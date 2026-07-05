0c6cf37a5216404689af7725a0b1c1428906ba61
Komsomol39 <100467630+Komsomol39@users.noreply.github.com>
2026-07-06 00:28:18 +0200

    video player (media3 + ffmpeg decoder)

---
+++ app/src/main/java/com/komsomol/rustream/ui/screens/video/VideoViewModel.kt
@@ -0,0 +1,21 @@
+package com.komsomol.rustream.ui.screens.video
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.komsomol.rustream.data.video.VideoRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class VideoViewModel @Inject constructor(
+    private val repo: VideoRepository
+) : ViewModel() {
+
+    val videos   = repo.videos
+    val scanning = repo.scanning
+
+    init { refresh() }
+
+    fun refresh() = viewModelScope.launch { repo.scan() }
+}
